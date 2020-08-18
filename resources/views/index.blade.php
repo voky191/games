e17bcb00eadfff8a1d7ac6171fd3813aeda6225c
voky191 <voky191@gmail.com>
2020-08-18 11:48:46 +0300

    single game template, image fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,22 +4,24 @@
     <div class="container mx-auto px-4">
         <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Popular games</h2>
         <div class="popular-games text-sm grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 xl:grid-cols-6 gap-12 border-b border-gray-800 pb-16">
-            @foreach ($games as $game)
-            <div class="game mt-8">
-                <div class="relative inline-block">
-                    <a href="#"><img src="{{ $game['cover']['url'] }}" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150"></a>
-                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right:-20px; bottom:-20px">
-                        <div class="font-semibold text-xs flex justify-center items-center h-full">{{ round($game['aggregated_rating']) }}%</div>
+                @foreach ($popularGames as $game)
+                    <div class="game mt-8">
+                        <div class="relative inline-block">
+                            <a href="#"><img src="{{ Str::replaceFirst('thumb', 'cover_big', $game['cover']['url']) }}" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150"></a>
+                            <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right:-20px; bottom:-20px">
+                                <div class="font-semibold text-xs flex justify-center items-center h-full">{{ round($game['aggregated_rating']).'%' }}</div>
+                            </div>
+                        </div>
+                        <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">{{$game->name}}</a>
+                       @foreach ($game['platforms'] as $platform)
+                           {{-- @if (array_key_exists('abbreviation', $platform))--}}
+                            <div class="text-gray-400 mt-1" style="display: inline-block">
+                                {{ $platform['name'] }}
+                            </div>
+                          {{--  @endif --}}
+                        @endforeach
                     </div>
-                </div>
-                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">{{$game->name}}</a>
-                @foreach ($game['platforms'] as $platform)
-                <div class="text-gray-400 mt-1" style="display: inline-block">
-                    {{ $platform }}
-                </div>
                 @endforeach
-            </div>
-            @endforeach
         </div>
         <div class="flex flex-col lg:flex-row my-10">
             <div class="recently-reviewed w-full lg:w-3/4 mr-0 lg:mr-32">
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'GameController@index');
+Route::get('/game', 'GameController@show');
